fbead25ff51557ed27719a93d69d167a2e54b6eb
Mehraz Morshed <mehrazmorshed@gmail.com>
2024-10-05 21:26:32 +0600

    modify theme files

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package news24
+ * @package News24
  * @since 1.0
  */
 
--- single.php
+++ single.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
- * @package news24
+ * @package News24
  * @since 1.0
  */
 
