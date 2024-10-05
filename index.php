18c5128298a2b224769f46d29cc7ba4e2a4eca8e
Mehraz Morshed <mehrazmorshed@gmail.com>
2024-10-05 19:57:25 +0600

    featured image added

--- index.php
+++ index.php
@@ -33,36 +33,48 @@ get_header();
             endif;
 
             echo '<div id="dotted"></div>';
-            while ( have_posts() ) : the_post();
+
+
+while ( have_posts() ) : the_post();
+    ?>
+    <article class="dotted post-wrapper" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="post-thumbnail-wrapper">
+            <?php if ( has_post_thumbnail() ) : // Display featured image ?>
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('medium'); ?>
+                </a>
+            <?php endif; ?>
+        </div><!-- .post-thumbnail-wrapper -->
+
+        <div class="post-content-wrapper">
+            <header class="entry-header">
+                <?php
+                if ( is_singular() ) :
+                    the_title( '<h1 class="entry-title">', '</h1>' );
+                else :
+                    the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+                endif;
                 ?>
-                <article class="dotted" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
 
-                        <?php
-                        if ( is_singular() ) :
-                            the_title( '<h1 class="entry-title">', '</h1>' );
-                        else :
-                            the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-                        endif;
-                        ?>
+                <div class="entry-meta">
+                    <?php news24_posted_on(); ?>
+                </div><!-- .entry-meta -->
 
-                        <div class="entry-meta">
-                            <?php news24_posted_on(); ?>
-                        </div><!-- .entry-meta -->
+            </header><!-- .entry-header -->
 
-                    </header><!-- .entry-header -->
+            <div class="entry-content">
+                <?php the_excerpt(); ?>
+            </div><!-- .entry-content -->
 
-                    <div class="entry-content">
-                        <?php the_excerpt(); ?>
-                    </div><!-- .entry-content -->
+            <footer class="entry-footer">
+                <?php news24_entry_footer(); ?>
+            </footer><!-- .entry-footer -->
+        </div><!-- .post-content-wrapper -->
+    </article><!-- #post-<?php the_ID(); ?> -->
+<?php
+endwhile;
 
-                    <footer class="entry-footer">
-                        <?php news24_entry_footer(); ?>
-                    </footer><!-- .entry-footer -->
 
-                </article><!-- #post-<?php the_ID(); ?> -->
-            <?php
-            endwhile;
 
             // pagination
 
